a30488d44eacf198f7b11e96348f00fc1e5af7dc
Sebastian Kvalvåg <82176302+Sebbe92@users.noreply.github.com>
2025-09-26 16:31:08 +0200

    created routes, views and swapped to ES6

--- index.js
+++ index.js
@@ -1,27 +1,75 @@
-const express = require('express');
-const axios = require('axios');
-const app = express();
+import 'dotenv/config';
+import express from 'express';
+import axios from 'axios';
+import pug from 'pug';
 
+
+
+const app = express();
+const currentDir = process.cwd();
 app.set('view engine', 'pug');
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(currentDir + '/public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // * Please DO NOT INCLUDE the private app access token in your repo. Don't do this practicum in your normal account.
-const PRIVATE_APP_ACCESS = '';
+const CUSTOM_DOGS_HS_TOKEN = process.env.CUSTOM_DOGS_HS_TOKEN;
 
 // TODO: ROUTE 1 - Create a new app.get route for the homepage to call your custom object data. Pass this data along to the front-end and create a new pug template in the views folder.
 
-// * Code for Route 1 goes here
+app.get('/', async (req, res) => {
+    const properties = 'name,breed,age';
+    try {
+        const dogObjectsUrl = 'https://api.hubapi.com/crm/v3/objects/2-192641628?properties=' + properties;
+        const headers = {
+            Authorization: `Bearer ${CUSTOM_DOGS_HS_TOKEN}`,
+            'Content-Type': 'application/json'
+        };
+        const response = await axios.get(dogObjectsUrl, { headers });
+        const result = response.data.results;
+        res.render('index', { title: 'Custom Dogs | HubSpot APIs', result });
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Error rendering page');
+    }
+});
 
 // TODO: ROUTE 2 - Create a new app.get route for the form to create or update new custom object data. Send this data along in the next route.
 
-// * Code for Route 2 goes here
+app.get('/update', async (req, res) => {
+    try {
+        res.render('update', { title: 'Custom Dogs | Update Dog' });
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Error rendering page');
+    }
+});
 
 // TODO: ROUTE 3 - Create a new app.post route for the custom objects form to create or update your custom object data. Once executed, redirect the user to the homepage.
 
-// * Code for Route 3 goes here
+app.post('/update-cobj', async (req, res) => {
+    const newDog = {
+        properties: {
+            "name": "Buddy",
+            "breed": "Golden Retriever",
+            "age": "3"
+        }
+    };
+    
+    const createDogUrl = 'https://api.hubapi.com/crm/v3/objects/2-192641628';
+    const headers = {
+        Authorization: `Bearer ${CUSTOM_DOGS_HS_TOKEN}`,
+        'Content-Type': 'application/json'
+    };
 
+    try {
+        await axios.post(createDogUrl, newDog, { headers });
+        res.redirect('/update');
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Error creating/updating dog');
+    }
+});
 /** 
 * * This is sample code to give you a reference for how you should structure your calls. 
 
